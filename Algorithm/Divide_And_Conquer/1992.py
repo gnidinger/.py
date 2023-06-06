7908a3fcf4c303130b3f58dd1e1d02165e33b63b
gnidinger <gcnb87@gmail.com>
2023-06-06 12:23:33 +0900

    commit

--- Algorithm/Divide_And_Conquer/1992.py
+++ Algorithm/Divide_And_Conquer/1992.py
@@ -1,39 +1,36 @@
 import sys
 
 n = int(sys.stdin.readline().rstrip())
-white = 0
-blue = 0
-board = [list(map(int, sys.stdin.readline().rstrip().split())) for _ in range(n)]
+video = [list(map(int, list(sys.stdin.readline().rstrip()))) for _ in range(n)]
+result = ''
 
 
 def is_same(row, col, size):
-    color = board[row][col]
+    color = video[row][col]
     for i in range(row, row + size):
         for j in range(col, col + size):
-            if board[i][j] != color:
+            if video[i][j] != color:
                 return False
     return True
 
 
 def quadrant(row, col, size):
-    global white, blue
+    global result
+
     if is_same(row, col, size):
-        if board[row][col] == 0:
-            white += 1
-            return
-        else:
-            blue += 1
-            return
+        result += str(video[row][col])
+        return
 
     new_size = size // 2
 
+    result += '('
     quadrant(row, col, new_size)
     quadrant(row, col + new_size, new_size)
     quadrant(row + new_size, col, new_size)
     quadrant(row + new_size, col + new_size, new_size)
+    result += ')'
 
 
 quadrant(0, 0, n)
 
-print(white)
-print(blue)
+print(result)
