c727193753f233a1e76858c798ad763cbc04d588
gnidinger <gcnb87@gmail.com>
2023-08-15 21:08:30 +0900

    commit

--- strategy_pattern/main.py
+++ strategy_pattern/main.py
@@ -6,8 +6,8 @@ lst = [4, 16, 31, 5, 4, 17, 1, 10, 15, 3, 16, 6, 7, 2, 2, 1, 5, 13, 17, 14, 4, 0
 
 sorter = Sorter(QuickSortStrategy())
 sorter.sort(lst)
-print(lst)
+print("Quick Sort: ", lst)
 
 sorter.set_strategy(MergeSortStrategy())
 sorter.sort(lst)
-print(lst)
+print("Merge Sort: ", lst)
