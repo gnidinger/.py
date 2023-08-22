96bb54a1dbee52a4fe91113f5710f88b58881c71
gnidinger <gnidinger@gmail.com>
2023-08-23 00:04:37 +0900

    commit

--- Algorithm/test.py
+++ Algorithm/test.py
@@ -1,6 +1,27 @@
-fruits = ['apple', 'banana', 'orange']
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
 
-# enumerate를 이용해 딕셔너리 생성
-enumerated_dict = {index: fruit for index, fruit in enumerate(fruits)}
+train_data = datasets.MNIST(root="data", train=True, download=True, transform=transforms.ToTensor())
+test_data = datasets.MNIST(root="data", train=False, transform=transforms.ToTensor())
 
-print(enumerated_dict)
+train_loader = DataLoader(train_data, batch_size=64, shuffle=True)
+test_loader = DataLoader(test_data, batch_size=64, shuffle=False)
+
+
+class MLP(nn.Module):
+    def __init__(self):
+        super(MLP, self).__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(28 * 28, 64), nn.ReLU(), nn.Linear(64, 64), nn.ReLU(), nn.Linear(64, 10)
+        )
+
+    def forward(self, x):
+        x = x.view(x.size(0), -1)
+        return self.layers(x)
+
+
+model = MLP()
